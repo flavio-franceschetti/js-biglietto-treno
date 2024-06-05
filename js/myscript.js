4337fd06efc48b2f25bfcf80938a2eda5027c08b
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-05 07:46:20 +0200

    feat: calculation of discounts for minors and those over 65

--- js/myscript.js
+++ js/myscript.js
@@ -15,3 +15,19 @@ console.log("Chilometri che vanno percorsi: " + travelKm);
 //Calcolo il costo totale del viaggio totale km * 0,21€
 let travelPrice = travelKm * 0.21;
 console.log("Prezzo totale senza sconti: " + travelPrice);
+
+//Togliamo il 20% da costo del biglietto ai ragazzi >18
+if (userAge < 18) {
+  //calcolo del valore dello sconto
+  const discount20 = (travelPrice * 20) / 100;
+  //calcolo del prezzo finale scontato
+  var finalPrice = travelPrice - discount20;
+  console.log("Quantità sconto: " + discount20, "Prezzo finale: " + finalPrice);
+  //Togliamo il 40% dal costo del biglietto alle persone over 65
+} else if (userAge >= 65) {
+  //calcoliamo il valore dello sconto
+  const discount40 = (travelPrice * 40) / 100;
+  //calcoliamo il prezzo finale
+  var finalPrice = travelPrice - discount40;
+  console.log("Quantità sconto: " + discount40, "Prezzo finale: " + finalPrice);
+}
