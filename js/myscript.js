65cc35fac2fad6b0c307927f1b66be0bc2691b05
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-06 15:04:25 +0200

    feat: FINE CE L'HO FATTA (almeno credo)

--- js/myscript.js
+++ js/myscript.js
@@ -14,20 +14,41 @@ console.log("Chilometri che vanno percorsi: " + travelKm);
 
 //Calcolo il costo totale del viaggio totale km * 0,21€
 let travelPrice = travelKm * 0.21;
+travelPrice = parseFloat(travelPrice.toFixed(2));
 console.log("Prezzo totale senza sconti: " + travelPrice);
+
+//Dichiaro la variabile del prezzo finale
+let finalPrice;
 
 //Togliamo il 20% da costo del biglietto ai ragazzi >18
 if (userAge < 18) {
+  //se l'user è minorenne
   //calcolo del valore dello sconto
-  const discount20 = (travelPrice * 20) / 100;
+  let discount20 = (travelPrice * 20) / 100;
+  discount20 = parseFloat(discount20.toFixed(2));
+
   //calcolo del prezzo finale scontato
-  var finalPrice = travelPrice - discount20;
+  finalPrice = travelPrice - discount20;
+  finalPrice = parseFloat(finalPrice.toFixed(2));
+
   console.log("Quantità sconto: " + discount20, "Prezzo finale: " + finalPrice);
   //Togliamo il 40% dal costo del biglietto alle persone over 65
 } else if (userAge >= 65) {
+  //se l'utente è over 65
   //calcoliamo il valore dello sconto
-  const discount40 = (travelPrice * 40) / 100;
+  let discount40 = (travelPrice * 40) / 100;
+  discount40 = parseFloat(discount40.toFixed(2));
+
   //calcoliamo il prezzo finale
-  var finalPrice = travelPrice - discount40;
+  finalPrice = travelPrice - discount40;
+  finalPrice = parseFloat(finalPrice.toFixed(2));
+
   console.log("Quantità sconto: " + discount40, "Prezzo finale: " + finalPrice);
+} else {
+  // altrimenti nessuno sconto tra i 18 e i 64 anni
+  finalPrice = travelPrice;
+
+  console.log("Prezzo finale: " + finalPrice);
 }
+//STAMPIAMO NELL HTML IL PREZZO FINALE
+document.querySelector(".finalPrice").innerText = finalPrice;
